e10c1e2f63e7fcddaeb0ef6eef17449550a2886d
amasyooh <83514394+amasyooh@users.noreply.github.com>
2024-04-21 16:22:18 +0300

    Update Task_6_Parse2.py

--- Task_6_Parse2.py
+++ Task_6_Parse2.py
@@ -1,30 +1,37 @@
 from bs4 import BeautifulSoup
+
 def update_image_source(html_content, new_image_path):
-# Parse the HTML content
-soup
-BeautifulSoup (html_content, 'html.parser')
-# Find the <img> tag by its id
-logo img = soup.find('img', {'id': 'logo'})
-if logo_img:
-else:
-# Update the 'src' attribute with the new image path
-logo_img['src'] = new_image_path
-print("Image source updated.")
-print("No image found with the specified id.")
-# Convert the soup object back to a string
-updated_html = str(soup)
-return updated_html
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find the <img> tag by its id
+    logo_img = soup.find('img', {'id': 'logo'})
+    if logo_img:
+        # Update the 'src' attribute with the new image path
+        logo_img['src'] = new_image_path
+        print("Image source updated.")
+    else:
+        print("No image found with the specified id.")
+
+    # Convert the soup object back to a string
+    updated_html = str(soup)
+    return updated_html
+
 # Example HTML content
-html content = ***
+html_content = '''
 <html>
-<head></head>
-<body>
-<img id="logo" src="old_logo.png">
-</body>
+    <head></head>
+    <body>
+        <img id="logo" src="old_logo.png">
+    </body>
 </html>
+'''
+
 # New image path
 new_image_path = 'new_logo.png'
+
 # Update the image source
 updated_html = update_image_source(html_content, new_image_path)
+
 print("Updated HTML:")
-print (updated_html)
+print(updated_html)
